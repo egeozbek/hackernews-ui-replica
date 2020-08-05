01b8d652a69bffd9b60356bd3be1e10d2e5f287d
Ege <egedenizi97@gmail.com>
2020-08-06 01:27:51 +0300

    ADD Footer Component at the end of the Website, FIX Minor Indexing problem at NewsListItem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {HttpClientModule} from "@angular/common/http";
 import { HeaderComponent } from './components/header/header.component';
 import { NewsListItemComponent } from './components/news-list-item/news-list-item.component';
 import { DateAgoPipe } from './pipes/date-ago.pipe';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NewsListItemComponent,
-    DateAgoPipe
+    DateAgoPipe,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
